4d66a4b0c60cef257a571626cdf57d5255584142
LH <lh@p50.top>
2025-08-12 22:31:37 +0700

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,14 @@
 if status is-interactive
-    bass source ~/.profile
-    bass source ~/.shell_common
+    # Set the default editor
+    export EDITOR='nano'
+    export VISUAL='lite-xl'
+
+    fish_add_path --append --path ~/.bin ~/.local/bin
+
+    # PATH changes
+    bass [[ -r ~/.profile ]] && bass source ~/.profile
+    # shell aliases
+    bass [[ -r ~/.shell_common ]] && bass source ~/.shell_common
 
     fastfetch
 end
